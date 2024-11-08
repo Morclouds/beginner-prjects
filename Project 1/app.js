47ea2eccd1c1227070ff4752b1356184b5bad02f
Morgan <143568735+Morclouds@users.noreply.github.com>
2024-11-07 18:12:22 -0600

    java fix

--- Project 1/app.js
+++ Project 1/app.js
@@ -1,11 +1,20 @@
-const happyFace = document.querySelector('.happy')
-const sadFace = document.querySelector('.sad')
+const happyFace = document.querySelector('.happy');
+const sadFace = document.querySelector('.sad');
 
 
 // add event listener
-happyFace.addEventListener('click',() => {
-    if (sadFace.classList.conatains('sad')){
-        sadFace.classList.add('active')
+happyFace.addEventListener('click', () => {
+    if (sadFace.classList.contains('sad')) {
+        sadFace.classList.add('active');
+        happyFace.classList.remove('active');
     }
 }
-)
+);
+
+sadFace.addEventListener('click', () => {
+    if (happyFace.classList.contains('happy')) {
+        happyFace.classList.add('active');
+        sadFace.classList.remove('active');
+    }
+}
+);
