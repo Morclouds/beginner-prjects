b20432bdd0c364cebf514b98e0bbfcdd8dc701a0
Morgan <143568735+Morclouds@users.noreply.github.com>
2024-11-08 20:33:26 -0600

    Update app.js

--- app.js
+++ app.js
@@ -1,3 +1,11 @@
-function openProject(projectFolder) {
-    window.location.href = `/beginner-projects/${projectFolder}/index.html`;
+function openProject(Project1) {
+    window.location.href = `${Project1}/index.html`;
+}
+
+function openProject(Project2) {
+    window.location.href = `${Project2}/index.html`;
+}
+
+function openProject(Project3) {
+    window.location.href = `${Project3}/index.html`;
 }
