064538e1eab98a6f40bb0bb01dca7432028db223
Morgan <143568735+Morclouds@users.noreply.github.com>
2024-11-08 18:21:42 -0600

    fin

---
+++ Project 2/app.js
@@ -0,0 +1,41 @@
+let data = [
+    {
+        name: 'Tyler', 
+        age:'33'
+    },
+
+   {
+    name: 'Kendrick', 
+    age:'37'
+   },
+
+    {
+        name: 'Scott', 
+        age:'40'
+    },
+    {
+        name: 'Steve', 
+        age:'26'
+    },
+   
+    {
+        name: 'Ravyn', 
+        age:'25'
+},
+   
+    {
+        name: 'Kelela', 
+        age:'41'
+    }
+    
+];
+
+const info = document.querySelector('#info')
+
+let details = data.map(function(item) {
+    return (
+        '<div>' + item.name + ' ' + 'is ' + item.age + ' years old' + '</div>')
+    
+});
+
+info.innerHTML = details.join('\n');
